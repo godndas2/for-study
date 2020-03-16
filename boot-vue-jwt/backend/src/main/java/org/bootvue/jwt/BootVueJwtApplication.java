6aafb409a2039e0dbbe3e31de74d9696f612f6d3
halfdev <gusgodnd@naver.com>
2020-03-17 01:25:23 +0900

    backend first commit

---
+++ boot-vue-jwt/backend/src/main/java/org/bootvue/jwt/BootVueJwtApplication.java
@@ -0,0 +1,13 @@
+package org.bootvue.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootVueJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootVueJwtApplication.class, args);
+    }
+
+}
---
+++ boot-vue-jwt/backend/src/test/java/org/bootvue/jwt/BootVueJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bootvue.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootVueJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
