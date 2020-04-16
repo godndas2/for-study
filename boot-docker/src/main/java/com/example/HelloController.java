222117e98dbd483e2197b7d852b2965fa8704270
halfdev <gusgodnd@naver.com>
2020-04-16 17:55:06 +0900

    WSL Docker Settings Done

--- boot-docker/src/main/java/com/example/HelloController.java
+++ boot-docker/src/main/java/com/example/HelloController.java
@@ -1,23 +1,12 @@
 package com.example;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
-    @RequestMapping("/greeting")
-    public Hello greeting(@RequestParam(value="name", defaultValue="World") String name) {
-        return new Hello(counter.incrementAndGet(), String.format(template, name));
-    }
-
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
