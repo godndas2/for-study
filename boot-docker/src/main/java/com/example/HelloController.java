9ac4e675459203367b5f59b38959d925d0a5a923
halfdev <gusgodnd@naver.com>
2020-04-17 01:07:18 +0900

    Done

--- boot-docker/src/main/java/com/example/HelloController.java
+++ boot-docker/src/main/java/com/example/HelloController.java
@@ -1,13 +1,15 @@
 package com.example;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/")
 public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings Docker & Spring Boot!";
     }
 }
