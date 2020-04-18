df5b9ad8b32440386973d98f176c8839cc1a37e2
halfdev <gusgodnd@naver.com>
2020-04-18 23:49:11 +0900

    batch basic start

---
+++ boot-batch-jpa/src/main/java/com/batch/SpringbootBatchJpaApplication.java
@@ -0,0 +1,13 @@
+package com.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBatchJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBatchJpaApplication.class, args);
+    }
+
+}
---
+++ boot-batch-jpa/src/test/java/com/batch/SpringbootBatchJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBatchJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
