19a1284fcb5aa16dd466f9acb77bb4a8d7dd7b28
halfdev <gusgodnd@naver.com>
2020-04-19 23:40:30 +0900

    boot redis basic

---
+++ boot-redis-basic/src/main/java/com/redis/BootRedisBasicApplication.java
@@ -0,0 +1,13 @@
+package com.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRedisBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRedisBasicApplication.class, args);
+    }
+
+}
---
+++ boot-redis-basic/src/test/java/com/redis/BootRedisBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRedisBasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
