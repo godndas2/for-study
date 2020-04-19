f85201c52fba83087ec69820168a0c746db37b3e
halfdev <gusgodnd@naver.com>
2020-04-20 00:44:48 +0900

    edde825ba0b437698f23ad6ee5141f5a1678564c 방법(1)

--- boot-redis-basic/src/main/java/com/redis/config/RedisCacheConfig.java
+++ boot-redis-basic/src/main/java/com/redis/config/RedisCacheConfig.java
@@ -1,23 +1,34 @@
 package com.redis.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import redis.embedded.RedisServer;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 
 @Configuration
-@EnableCaching
+//@EnableCaching
 @RequiredArgsConstructor
 public class RedisCacheConfig {
 
-    private final JedisConnectionFactory jedisConnectionFactory;
+    private final RedisProperties redisProperties;
+    private RedisServer redisServer;
 
-    @Bean
-    public RedisCacheManager redisCacheManager() {
-        return RedisCacheManager.create(jedisConnectionFactory);
+
+
+    @PostConstruct
+    public void redisServer() throws IOException {
+        redisServer = new RedisServer(redisProperties.getPort());
+        redisServer.start();
     }
+
+//    private final JedisConnectionFactory jedisConnectionFactory;
+//
+//    @Bean
+//    public RedisCacheManager redisCacheManager() {
+//        return RedisCacheManager.create(jedisConnectionFactory);
+//    }
 
 
 }
--- boot-redis-basic/src/main/java/com/redis/config/RedisConfig.java
+++ boot-redis-basic/src/main/java/com/redis/config/RedisConfig.java
@@ -3,11 +3,14 @@ package com.redis.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisServer;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+import javax.annotation.PostConstruct;
 
 @RequiredArgsConstructor
 @Configuration
@@ -16,21 +19,18 @@ public class RedisConfig {
 
     private final RedisProperties redisProperties;
 
+    // jedis
     @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
-        redisStandaloneConfiguration.setHostName(redisProperties.getHost());
-        redisStandaloneConfiguration.setPort(redisProperties.getPort());
-        redisStandaloneConfiguration.setDatabase(0);
-        redisStandaloneConfiguration.setPassword(RedisPassword.none());
-
-        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
+        return new JedisConnectionFactory(config);
     }
 
     @Bean
     public RedisTemplate<?, ?> redisTemplate() {
         RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
         return redisTemplate;
     }
+
 }
