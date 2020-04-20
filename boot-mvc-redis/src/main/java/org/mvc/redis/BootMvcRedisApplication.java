f35f7dd90dfed465d9da1a886e09c3c69719bec0
halfdev <gusgodnd@naver.com>
2020-04-20 23:25:44 +0900

    springboot mvc redis start

---
+++ boot-mvc-redis/src/main/java/org/mvc/redis/BootMvcRedisApplication.java
@@ -0,0 +1,13 @@
+package org.mvc.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootMvcRedisApplication.class, args);
+    }
+
+}
