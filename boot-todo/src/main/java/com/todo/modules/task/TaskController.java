f5c93354860c79a4a9e19b46ff900f677474923a
halfdev <gusgodnd@naver.com>
2020-04-28 00:07:10 +0900

    login 된 account 출력

--- boot-todo/src/main/java/com/todo/modules/task/TaskController.java
+++ boot-todo/src/main/java/com/todo/modules/task/TaskController.java
@@ -9,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
