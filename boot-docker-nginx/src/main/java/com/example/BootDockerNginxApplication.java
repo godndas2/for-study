c1e7f2414853f6b8efb19bd0a9c1aa2a94afd249
halfdev <gusgodnd@naver.com>
2020-05-28 14:31:25 +0900

    TODO, Docker Build

--- boot-docker-nginx/src/main/java/com/example/BootDockerNginxApplication.java
+++ boot-docker-nginx/src/main/java/com/example/BootDockerNginxApplication.java
@@ -1,4 +1,4 @@
-package org.docker.nginx;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- boot-docker-nginx/src/main/java/com/example/IndexController.java
+++ boot-docker-nginx/src/main/java/com/example/IndexController.java
@@ -1,9 +1,9 @@
-package org.docker.nginx;
+package com.example;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
 
     @GetMapping("/")
