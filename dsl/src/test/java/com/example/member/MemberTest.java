e7ae8e2f1aa64ef1f3b879267c51181a5efd8378
huhyun <gusgodnd@naver.com>
2022-03-11 00:57:50 +0900

    MemberTest

--- dsl/src/test/java/com/example/member/MemberTest.java
+++ dsl/src/test/java/com/example/member/MemberTest.java
@@ -1,14 +1,20 @@
 package com.example.member;
 
 import com.example.team.Team;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -18,8 +24,8 @@ public class MemberTest {
     @Autowired
     EntityManager em;
 
-    @Test
-    public void testEntity() {
+    @BeforeEach
+    public void init() {
         Team teamA = new Team("teamA");
         Team teamB = new Team("teamB");
         em.persist(teamA);
@@ -32,15 +38,36 @@ public class MemberTest {
         em.persist(memberTest2);
         em.persist(memberTest3);
         em.persist(memberTest4);
+    }
 
+    @AfterEach
+    public void tearDown() {
         em.flush();
         em.clear();
+    }
 
+    @Test
+    @DisplayName("Entity")
+    public void testEntity() {
         List<Member> members = em.createQuery("select m from Member m", Member.class).getResultList();
 
         for (Member member : members) {
             System.out.println("members=" + member);
             System.out.println("-> member.team=" + member.getTeam());
         }
+    }
+
+    @Test
+    @DisplayName("JPQL")
+    public void jpql() {
+        String members =
+                "select m from Member m " +
+                "where m.username = :username";
+
+        Member findMember = em.createQuery(members, Member.class)
+                .setParameter("username", "member1")
+                .getSingleResult();
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
     }
 }
